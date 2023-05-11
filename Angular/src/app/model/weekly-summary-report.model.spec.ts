3390eb02d9ffd83222df32edc6655f681f4fd3a2
Tejashree Gunjal <tejashree_gunjal@persistent.com>
2023-05-11 10:06:04 +0530

    WeeklySummaryReport DTO class modified

---
+++ Angular/src/app/model/weekly-summary-report.model.spec.ts
@@ -0,0 +1,7 @@
+import { WeeklySummaryReport } from './weekly-summary-report.model';
+
+describe('WeeklySummaryReport', () => {
+  it('should create an instance', () => {
+    expect(new WeeklySummaryReport()).toBeTruthy();
+  });
+});
--- WeeklyReportAPI/WeeklyReportAPI/DTO/WeeklySummaryReport.cs
+++ WeeklyReportAPI/WeeklyReportAPI/DTO/WeeklySummaryReport.cs
@@ -4,9 +4,8 @@ namespace WeeklyReportAPI.DTO
 {
     public class WeeklySummaryReport
     {
-        public WSR_ActionItems ActionItem { get; set; }
         public WSR_SummaryDetails Summary { get; set; }
-        public WSR_Teams Team { get; set; }
-
+        public List<WSR_ActionItems> ActionItems { get; set; }        
+        public List<WSR_Teams> Teams { get; set; }
     }
 }
