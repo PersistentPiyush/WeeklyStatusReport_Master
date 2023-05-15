5d342bead51187ff9a0121b45721d5c40299a6ff
Tejashree Gunjal <tejashree_gunjal@persistent.com>
2023-05-15 10:53:40 +0530

    DB script updated

--- Database/WSR_DBscript.sql
+++ Database/WSR_DBscript.sql
@@ -1,7 +1,7 @@
 USE [wsr]
 GO
 
-/****** Object:  Table [dbo].[WSR_Teams]    Script Date: 5/10/2023 6:52:53 PM ******/
+/****** Object:  Table [dbo].[WSR_Teams]    Script Date: 5/15/2023 10:52:24 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,45 +9,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[WSR_Teams](
-	[TeamID] [int] IDENTITY(1,1) NOT NULL,
+	[TeamID] [int] NOT NULL,
+	[TeamName] [nvarchar](100) NULL,
 	[SummaryID] [int] NULL,
-	[Name] [nvarchar](100) NULL,
+	[LeadName] [nvarchar](100) NULL,
 	[TaskCompleted] [nvarchar](max) NULL,
 	[TaskInProgress] [nvarchar](max) NULL,
-	[CurrentWeekPlan] [nvarchar](max) NULL,
-	[NoOfTaskCompleted] [int] NULL,
-	[NoOfTaskInProgress] [int] NULL,
- CONSTRAINT [PK_WSR_Teams] PRIMARY KEY CLUSTERED 
-(
-	[TeamID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-USE [wsr]
-GO
-
-/****** Object:  Table [dbo].[WSR_SummaryDetails]    Script Date: 5/10/2023 6:52:45 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[WSR_SummaryDetails](
-	[SummaryID] [int] IDENTITY(1,1) NOT NULL,
-	[Overall] [nvarchar](max) NULL,
-	[OverallStatus] [char](1) NULL,
-	[Schedule] [nvarchar](max) NULL,
-	[ScheduleStatus] [char](1) NULL,
-	[Resource] [nvarchar](max) NULL,
-	[ResourceStatus] [char](1) NULL,
-	[Risk] [nvarchar](max) NULL,
-	[RiskStatus] [char](1) NULL,
-	[WeekEndingDate] [datetime] NULL,
-	[CreatedBy] [nvarchar](100) NULL,
-	[CreatedOn] [datetime] NULL,
-	[UpdatedBy] [nvarchar](100) NULL,
-	[UpdatedOn] [datetime] NULL
+	[CurrentWeekPlan] [nvarchar](max) NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
@@ -55,7 +23,7 @@ GO
 USE [wsr]
 GO
 
-/****** Object:  Table [dbo].[WSR_ActionItems]    Script Date: 5/10/2023 6:52:05 PM ******/
+/****** Object:  Table [dbo].[WSR_ActionItems]    Script Date: 5/15/2023 10:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -82,5 +50,37 @@ ALTER TABLE [dbo].[WSR_ActionItems] ADD  CONSTRAINT [DF_WSR_ActionItems_isActive
 GO
 
 
+USE [wsr]
+GO
+
+/****** Object:  Table [dbo].[WSR_SummaryDetails]    Script Date: 5/15/2023 10:52:18 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WSR_SummaryDetails](
+	[SummaryID] [int] IDENTITY(1,1) NOT NULL,
+	[Overall] [nvarchar](max) NULL,
+	[OverallStatus] [char](1) NULL,
+	[Schedule] [nvarchar](max) NULL,
+	[ScheduleStatus] [char](1) NULL,
+	[Resource] [nvarchar](max) NULL,
+	[ResourceStatus] [char](1) NULL,
+	[Risk] [nvarchar](max) NULL,
+	[RiskStatus] [char](1) NULL,
+	[WeekEndingDate] [datetime] NULL,
+	[CreatedBy] [nvarchar](100) NULL,
+	[CreatedOn] [datetime] NULL,
+	[UpdatedBy] [nvarchar](100) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[Name] [nvarchar](100) NULL,
+ CONSTRAINT [PK_WSR_SummaryDetails] PRIMARY KEY CLUSTERED 
+(
+	[SummaryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
 
